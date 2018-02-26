78e4c1e19a6be6fd84ae570dd9d6f1b8e67e999b
Scott Molsness <smolsness@gmail.com>
2018-02-26 11:57:02 -0800

    inital commit of authentication boilerplate

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+var flash = require('connect-flash');
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('./config/ppConfig');
+var isLoggedIn = require('./middleware/isLoggedIn');
+
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -9,11 +15,29 @@ app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(flash()); // run flash app after session app
+
+app.use(passport.initialize());  // make sure to configure passport and secret before initializing passport
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  // before every route, attach the flash messages and current user to res.locals
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/profile', function(req, res) {
+app.get('/profile', isLoggedIn, function(req, res) {
   res.render('profile');
 });
 
